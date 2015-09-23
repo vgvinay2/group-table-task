101d0d94f19ae593473c620cb6c9bef0a8864a50
vinay-career-builder <Vinay.KumarGupta@careerbuilder.com>
2015-09-23 16:54:16 +0530

    delete all templete from rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   resources :secret_codes
-  devise_for :users, :controllers => {:registrations => "users/registrations",
-                                      :sessions => "users/sessions"}
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
 
 
 
