e4588a57e451aaa86564076397cdad51721000d9
vinay-career-builder <Vinay.KumarGupta@careerbuilder.com>
2015-09-23 16:37:14 +0530

    Modify devise gemaa... :first_quarter_moon_with_face:

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,7 @@
-class RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
 
   def new
     super
-  end
-
-  def create
-    if params[:secret_code].present? && params[:secret_code]=="foobar"
-      build_resource(sign_up_params)
-      super
-    else
-      redirect_to :back
-    end
   end
 
   private
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
 
   def new
     super
